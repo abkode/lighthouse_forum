2b4aa32c657e500f0c3da25ecc171d6e01554ba4
abkode <abozar.rahimi@gmail.com>
2016-08-14 23:22:47 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Lighthouse Forum
+
+Welcome to the example repo for your first Rails application!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '71fec4bfa2c1d9d10b8db69226fa4b3716feb8e17351ff119a298c19d36b300bc455452f5e11d1ec09fddb74b72eb4aa5e967a1ecb60c48a38888846a3348dc5'
